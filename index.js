bd5d2183b35bf33fb63e502470ce71e93f139fd4
sym-dev <37952374+sym-dev@users.noreply.github.com>
2021-01-14 20:09:48 +0900

    投稿ができるぞ！！！！

--- index.js
+++ index.js
@@ -1,26 +1,81 @@
-require('dotenv').config();
-const fetch = require('node-fetch');
-const URL = 'https://misskey.io/api/notes/show';
+const { v4: uuidv4 } = require("uuid");
+const open = require("open");
+const fs = require("fs");
+require("dotenv").config();
+const prompts = require("prompts");
+const fetch = require("node-fetch");
+const instanceURL = "https://misskey.io/";
 
-const body = {
-  "noteId": "8grh16g2hy"
-};
+const envPath = "./env";
 
-fetch(URL, {
-    method: 'POST',
+if (!fs.existsSync(envPath) && typeof process.env.UserToken == "undefined") {
+  const uuid = uuidv4();
+  const name = "MYSSKEY";
+  const authUrl =
+    instanceURL +
+    "miauth/" +
+    uuid +
+    "?" +
+    "name=" +
+    name +
+    "&permission=read:account,write:account,write:notes";
+  open(authUrl);
+  const tokenUrl = instanceURL + "api/miauth/" + uuid + "/check";
+  (async () => {
+    await prompts({
+      type: "text",
+      name: "value",
+      message: "認証後、Enterを押してください。",
+    });
+  })().then(() => {
+    fetch(tokenUrl, {
+      method: "POST",
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`${res.status} ${res.statusText}`);
+        }
+        return res.json();
+      })
+      .then((text) => {
+        fs.writeFile(".env", "UserToken=" + text.token, (err) => {
+          if (err) throw err;
+          console.log("正常に書き込みが完了しました");
+        });
+      })
+      .catch((err) => console.error(err));
+  });
+}
+
+(async () => {
+  const response = await prompts({
+    type: "text",
+    name: "noteText",
+    message: "何をお考えですか？",
+  });
+  return response;
+})().then((response) => {
+  const body = {
+    i: process.env.UserToken,
+    text: response.noteText,
+  };
+  fetch(instanceURL + "api/notes/create", {
+    method: "POST",
     body: JSON.stringify(body),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   })
-  .then(res => {
-    if (!res.ok) {
-      // 200 系以外のレスポンスはエラーとして処理
-      throw new Error(`${res.status} ${res.statusText}`);
-    }
-    return res.json();
-  })
-  // これがレスポンス本文のテキスト
-  .then(text => console.log(text))
-  // エラーはここでまとめて処理
-  .catch(err => console.error(err));
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`${res.status} ${res.statusText}`);
+      }
+      return res.json();
+    })
+    .then((text) => {
+      console.log("post succeed!");
+      console.log(text.createdNote.user.name);
+      console.log(text.createdNote.text);
+    })
+    .catch((err) => console.error(err));
+});
