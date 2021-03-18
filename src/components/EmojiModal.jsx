ae5044f9014326578dd00d5455231e73ab3548e4
sym-dev <37952374+sym-dev@users.noreply.github.com>
2021-03-18 13:33:21 +0900

    - 絵文字モーダルでの絵文字検索

--- src/components/EmojiModal.jsx
+++ src/components/EmojiModal.jsx
@@ -18,7 +18,8 @@ export default function EmojiModal() {
     noteId,
     updateNoteId,
   } = useEmojiModalContext();
-  const { register, handleSubmit, setValue } = useForm();
+  const { register, watch, handleSubmit, setValue } = useForm();
+  const search = watch("searchEmoji");
   const onSubmitReaction = (data) => {
     let createReactionObject = {
       type: "api",
@@ -28,24 +29,7 @@ export default function EmojiModal() {
         data: {
           i: localStorage.getItem("UserToken"),
           noteId: noteId,
-          reaction: data.reaction,
-        },
-      },
-    };
-    socketRef.current.send(JSON.stringify(createReactionObject));
-    updateNoteId("");
-    updateEmojiModal(false);
-  };
-  const onSubmitReactionByEmoji = (data) => {
-    let createReactionObject = {
-      type: "api",
-      body: {
-        id: "reaction",
-        endpoint: "notes/reactions/create",
-        data: {
-          i: localStorage.getItem("UserToken"),
-          noteId: noteId,
-          reaction: ":" + data.emoji + ":",
+          reaction: data.reaction ? data.reaction : ":" + data.emoji + ":",
         },
       },
     };
@@ -64,7 +48,6 @@ export default function EmojiModal() {
         content: {
           position: "absolute",
           top: emojiModalPlace.y + 20,
-          left: emojiModalPlace.x - 200,
         },
       }}
       className="emojiModal"
@@ -79,64 +62,96 @@ export default function EmojiModal() {
             placeholder={"input emoji..."}
             onSubmit={handleSubmit(onSubmitReaction)}
             required
-          ></input>
+          />
           <button onSubmit={handleSubmit(onSubmitReaction)}>
             <IoCheckmark />
           </button>
         </div>
       </form>
-      <form onSubmit={handleSubmit(onSubmitReactionByEmoji)}>
-        <div className="emojis">
-          <p>
-            <IoTime />
-            最近使用
-          </p>
-          <div className="RUEmoji">
-            {Array.isArray(RUEmoji) &&
-              RUEmoji.map((data) => (
-                <button
-                  key={data.id}
-                  onClick={() => {
-                    setValue("emoji", data.name);
-                    handleSubmit(onSubmitReactionByEmoji);
-                  }}
-                >
-                  <img src={data.url} alt={data.name} loading="lazy" />
-                  <input type="hidden" name="emoji" ref={register} />
-                </button>
-              ))}
-          </div>
-          {emojiCategory.map((cat) => (
-            <details key={cat}>
-              <summary>{cat}</summary>
-              <div className="emojisContainer">
-                {customEmojis
-                  .filter((emoji) => emoji.category === cat)
+      <div className="emojis">
+        <div className="emojisWrapper">
+          <form onSubmit={handleSubmit(onSubmitReaction)}>
+            <div className="emojisContainer">
+              {search &&
+                customEmojis
+                  .filter((emoji) => emoji.name.includes(search))
+                  .slice(0, 12)
                   .map((data) => (
                     <button
                       key={data.id}
                       onClick={() => {
-                        RUEmoji.unshift(data);
-                        if (RUEmoji.length > 10) {
-                          RUEmoji.pop();
-                        }
-                        localStorage.setItem(
-                          "RUEmoji",
-                          JSON.stringify(RUEmoji)
-                        );
                         setValue("emoji", data.name);
-                        handleSubmit(onSubmitReactionByEmoji);
+                        handleSubmit(onSubmitReaction);
                       }}
                     >
-                      <img src={data.url} alt={data.name} loading="lazy" />
                       <input type="hidden" name="emoji" ref={register} />
+                      <img src={data.url} alt={data.name} loading="lazy" />
                     </button>
                   ))}
-              </div>
-            </details>
-          ))}
+            </div>
+          </form>
+          <form onSubmit={handleSubmit(onSubmitReaction)}>
+            <p>
+              <IoTime />
+              最近使用
+            </p>
+            <div className="RUEmoji">
+              {Array.isArray(RUEmoji) &&
+                RUEmoji.map((data) => (
+                  <button
+                    key={data.id}
+                    onClick={() => {
+                      setValue("emoji", data.name);
+                      handleSubmit(onSubmitReaction);
+                    }}
+                  >
+                    <img src={data.url} alt={data.name} loading="lazy" />
+                    <input type="hidden" name="emoji" ref={register} />
+                  </button>
+                ))}
+            </div>
+            {emojiCategory.map((cat) => (
+              <details key={cat}>
+                <summary>{cat ? cat : "その他"}</summary>
+                <div className="emojisContainer">
+                  {customEmojis
+                    .filter((emoji) => emoji.category === cat)
+                    .map((data) => (
+                      <button
+                        key={data.id}
+                        onClick={() => {
+                          RUEmoji.unshift(data);
+                          if (RUEmoji.length > 10) {
+                            RUEmoji.pop();
+                          }
+                          localStorage.setItem(
+                            "RUEmoji",
+                            JSON.stringify(RUEmoji)
+                          );
+                          setValue("emoji", data.name);
+                          handleSubmit(onSubmitReaction);
+                        }}
+                      >
+                        <img src={data.url} alt={data.name} loading="lazy" />
+                        <input type="hidden" name="emoji" ref={register} />
+                      </button>
+                    ))}
+                </div>
+              </details>
+            ))}
+          </form>
         </div>
-      </form>
+        <input
+          type="text"
+          name="searchEmoji"
+          className="searchEmoji"
+          placeholder="検索"
+          ref={register}
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        />
+      </div>
     </Modal>
   );
 }
