f006615e8ca3fa2c9aec860b18ff8e330ab8a1b8
sym <37952374+sym-dev@users.noreply.github.com>
2021-03-25 00:03:19 +0900

    通知の種類を追加

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -3,7 +3,16 @@ import { useSocketContext } from "../utils/SocketContext";
 import { getRelativeTime } from "../utils/getRelativeTime";
 import Loading from "../components/Loading";
 import parseEmojis from "../utils/parseEmojis";
-import { IoAt, IoPersonAdd, IoRepeat, IoTime } from "react-icons/io5";
+import {
+  IoArrowUndo,
+  IoAt,
+  IoCheckmarkCircle,
+  IoPencil,
+  IoPeople,
+  IoPersonAdd,
+  IoRepeat,
+  IoTime,
+} from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 export default function RightBar() {
@@ -21,95 +30,221 @@ export default function RightBar() {
         <Loading />
       ) : (
         <div className="notificationWrapper">
-          {notifications.map((data) => {
-            let link = "";
-            let type = "";
-            let text = "";
-            let file = "";
-            let isNote = true;
-            switch (data.type) {
-              case "reaction":
-                link = data.note.id;
-                type = parseEmojis(data.reaction, data.note.emojis);
-                text = parseEmojis(data.note.text, data.note.emojis);
-                if (data.note.files.length > 0) {
-                  file = "(" + data.note.files.length + "つのファイル)";
-                }
-                break;
-              case "renote":
-                link = data.note.renote.id;
-                type = <IoRepeat className="renote" fontSize="1.3em" />;
-                text = parseEmojis(
-                  data.note.renote.text,
-                  data.note.renote.emojis
-                );
-                if (data.note.renote.files.length > 0) {
-                  file = "(" + data.note.renote.files.length + "つのファイル)";
-                }
-                break;
-              case "mention":
-                link = data.note.id;
-                type = <IoAt fontSize="1.3em" />;
-                text = parseEmojis(data.note.text, data.note.emojis);
-                if (data.note.files.length > 0) {
-                  file = "(" + data.note.files.length + "つのファイル)";
-                }
-                break;
-              case "follow":
-                type = <IoPersonAdd className="follow" fontSize="1em" />;
-                text = "フォローされました";
-                isNote = false;
-                break;
-              case "receiveFollowRequest":
-                type = <IoTime fontSize="1.3em" />;
-                text = "フォローリクエストされました";
-                isNote = false;
-                break;
-              default:
-                break;
-            }
-            return (
-              <div className="notificationContainer" key={data.id}>
-                <div className="notificationIcon">
-                  <img src={data.user.avatarUrl} alt={data.user.username} />
-                  <div className="type">{type}</div>
-                </div>
-                <div className="notificationBody">
-                  <div className="notificationNoteInfo">
-                    <Link
-                      to={
-                        data.user.host
-                          ? "/user/@" +
-                            data.user.username +
-                            "@" +
+          {notifications
+            .filter((data) => data.type !== "pollVote")
+            .map((data) => {
+              let link = "";
+              let type = "";
+              let reply = "";
+              let text = "";
+              let quote = "";
+              let file = "";
+              let isNote = true;
+              switch (data.type) {
+                case "follow":
+                  type = <IoPersonAdd className="follow" fontSize="1em" />;
+                  text = "フォローされました";
+                  isNote = false;
+                  break;
+                case "mention":
+                  link = data.note.id;
+                  type = <IoAt fontSize="1.3em" />;
+                  text = parseEmojis(data.note.text, data.note.emojis);
+                  if (data.note.files.length > 0) {
+                    file = "(" + data.note.files.length + "つのファイル)";
+                  }
+                  break;
+                case "reply":
+                  link = data.note.id;
+                  type = <IoArrowUndo fontSize="1.2em" />;
+                  reply = data.note.reply;
+                  text = parseEmojis(data.note.text, data.note.emojis);
+                  break;
+                case "renote":
+                  link = data.note.renote.id;
+                  type = <IoRepeat className="renote" fontSize="1.3em" />;
+                  text = parseEmojis(
+                    data.note.renote.text,
+                    data.note.renote.emojis
+                  );
+                  if (data.note.renote.files.length > 0) {
+                    file =
+                      "(" + data.note.renote.files.length + "つのファイル)";
+                  }
+                  break;
+                case "quote":
+                  link = data.note.id;
+                  type = <IoPencil className="renote" fontSize="1.2em" />;
+                  quote = data.note.renote;
+                  text = parseEmojis(data.note.text, data.note.emojis);
+                  break;
+                case "reaction":
+                  link = data.note.id;
+                  type = parseEmojis(data.reaction, data.note.emojis);
+                  text = parseEmojis(data.note.text, data.note.emojis);
+                  if (data.note.files.length > 0) {
+                    file = "(" + data.note.files.length + "つのファイル)";
+                  }
+                  break;
+                case "receiveFollowRequest":
+                  type = <IoTime fontSize="1.3em" />;
+                  text = "フォローリクエストされました";
+                  isNote = false;
+                  break;
+                case "followRequestAccepted":
+                  type = <IoCheckmarkCircle fontSize="1.3em" />;
+                  text = "フォローリクエストが承認されました";
+                  isNote = false;
+                  break;
+                case "groupInvited":
+                  console.log(data);
+                  type = <IoPeople fontSize="1.2em" />;
+                  text =
+                    "グループに招待されました: " + data.invitation.group.name;
+                  isNote = false;
+                  break;
+                case "app":
+                  break;
+                default:
+                  break;
+              }
+              return (
+                <>
+                  {reply && (
+                    <div className="replyContainer">
+                      <img src={reply.user.avatarUrl} />
+                      <div className="replyBody">
+                        <div className="replyInfo">
+                          <Link
+                            to={
+                              reply.user.host
+                                ? "/user/@" +
+                                  reply.user.username +
+                                  "@" +
+                                  reply.user.host
+                                : "/user/@" + reply.user.username
+                            }
+                            className="username"
+                          >
+                            {parseEmojis(
+                              reply.user.name
+                                ? reply.user.name
+                                : reply.user.username,
+                              reply.user.emojis
+                            )}
+                          </Link>
+                          {isNote ? (
+                            <Link
+                              to={"/notes/" + reply.id}
+                              className="createdAt"
+                            >
+                              {getRelativeTime(reply.createdAt)}
+                            </Link>
+                          ) : (
+                            <p className="createdAt">
+                              {getRelativeTime(reply.createdAt)}
+                            </p>
+                          )}
+                        </div>
+                        <div className="replyNoteBody">
+                          {parseEmojis(reply.text, reply.emojis)}
+                          {reply.files.length > 0 && (
+                            <span className="file">{`${reply.files.length}つのファイル`}</span>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                  <div className="notificationContainer" key={data.id}>
+                    <div className="notificationIcon">
+                      <img src={data.user.avatarUrl} alt={data.user.username} />
+                      <div className="type">{type}</div>
+                    </div>
+                    <div className="notificationBody">
+                      <div className="notificationNoteInfo">
+                        <Link
+                          to={
                             data.user.host
-                          : "/user/@" + data.user.username
-                      }
-                      className="username"
-                    >
-                      {parseEmojis(
-                        data.user.name ? data.user.name : data.user.username,
-                        data.user.emojis
-                      )}
-                    </Link>
-                    {isNote ? (
-                      <Link to={"/notes/" + link} className="createdAt">
-                        {getRelativeTime(data.createdAt)}
-                      </Link>
-                    ) : (
-                      <p className="createdAt">
-                        {getRelativeTime(data.createdAt)}
+                              ? "/user/@" +
+                                data.user.username +
+                                "@" +
+                                data.user.host
+                              : "/user/@" + data.user.username
+                          }
+                          className="username"
+                        >
+                          {parseEmojis(
+                            data.user.name
+                              ? data.user.name
+                              : data.user.username,
+                            data.user.emojis
+                          )}
+                        </Link>
+                        {isNote ? (
+                          <Link to={"/notes/" + link} className="createdAt">
+                            {getRelativeTime(data.createdAt)}
+                          </Link>
+                        ) : (
+                          <p className="createdAt">
+                            {getRelativeTime(data.createdAt)}
+                          </p>
+                        )}
+                      </div>
+                      <p className="notificationNoteBody">
+                        {text}
+                        {file && <span className="file">{file}</span>}
                       </p>
-                    )}
+                    </div>
                   </div>
-                  <p className="notificationNoteBody">
-                    {text}
-                    <span className="file">{file}</span>
-                  </p>
-                </div>
-              </div>
-            );
-          })}
+                  {quote && (
+                    <div className="quoteContainer">
+                      <img src={quote.user.avatarUrl} />
+                      <div className="quoteBody">
+                        <div className="quoteInfo">
+                          <Link
+                            to={
+                              quote.user.host
+                                ? "/user/@" +
+                                  quote.user.username +
+                                  "@" +
+                                  quote.user.host
+                                : "/user/@" + quote.user.username
+                            }
+                            className="username"
+                          >
+                            {parseEmojis(
+                              quote.user.name
+                                ? quote.user.name
+                                : quote.user.username,
+                              quote.user.emojis
+                            )}
+                          </Link>
+                          {isNote ? (
+                            <Link
+                              to={"/notes/" + quote.id}
+                              className="createdAt"
+                            >
+                              {getRelativeTime(quote.createdAt)}
+                            </Link>
+                          ) : (
+                            <p className="createdAt">
+                              {getRelativeTime(quote.createdAt)}
+                            </p>
+                          )}
+                        </div>
+                        <div className="quoteNoteBody">
+                          {parseEmojis(quote.text, quote.emojis)}
+                          {quote.files.length > 0 && (
+                            <span className="file">{`${quote.files.length}つのファイル`}</span>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                  <hr />
+                </>
+              );
+            })}
           <button
             className="motto"
             onClick={() => {
