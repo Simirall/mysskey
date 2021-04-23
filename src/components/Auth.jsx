4360d351e7dfe849d526b9bc721a2b174f4cc7b6
sym <37952374+sym-dev@users.noreply.github.com>
2021-04-23 12:06:17 +0900

    👿

--- src/components/Auth.jsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from "react-router-dom";
-
-const Auth = (props) =>
-  localStorage.getItem("isLogin") ? props.children : <Redirect to={"/login"} />;
-
-export default Auth;
--- src/components/Header.jsx
+++
@@ -1,6 +0,0 @@
-import { useHeaderContext } from "../utils/HeaderContext";
-
-export default function Header() {
-  const { headerValue } = useHeaderContext();
-  return <header className="top-header">{headerValue}</header>;
-}
