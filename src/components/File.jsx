66c6da07f7c3bd1916039b33724aa2629a2065eb
sym-dev <37952374+sym-dev@users.noreply.github.com>
2021-05-23 22:48:11 +0900

    動画を表示するように

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -46,6 +46,13 @@ export default function File({ data }) {
     file = (
       <audio src={data.url} title={data.name} preload="metadata" controls />
     );
+  } else if (/^video/.exec(data.type)) {
+    file = (
+      <video title={data.name} controls preload="metadata">
+        <source src={data.url} type={data.type} />
+        埋め込み動画に対応していないようです
+      </video>
+    );
   }
   return <>{file}</>;
 }
